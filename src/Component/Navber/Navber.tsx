c04d4f22d9125b8ff7570aa0432658233315dc97
Akahad1 <aksahad234567@gmail.com>
2025-01-28 22:23:02 +0600

    first commit

---
+++ src/Component/Navber/Navber.tsx
@@ -0,0 +1,9 @@
+const Navber = () => {
+  return (
+    <div>
+      <h1>Hello, Navber!</h1>
+    </div>
+  );
+};
+
+export default Navber;
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import Navber from "../../Component/Navber/Navber";
+
+const Home = () => {
+  return (
+    <div>
+      <Navber></Navber>
+    </div>
+  );
+};
+
+export default Home;
